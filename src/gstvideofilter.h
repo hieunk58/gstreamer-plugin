7498558056ac10a65d91eced3252793b1c310f22
hieunk <khachieunk@gmail.com>
2022-05-25 10:43:51 +0200

    define standard marcos

---
+++ src/gstvideofilter.h
@@ -0,0 +1,36 @@
+#ifndef _GST_VIDEO_FILTER_H_
+#define _GST_VIDEO_FILTER_H_
+
+#include <gst/gst.h>
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+typedef struct _GstVideoFilter {
+    GstElement element;
+
+    GstPad *sinkpad, *srcpad;
+
+    gboolean silent;
+
+} GstVideoFilter;
+
+
+typedef struct _GstVideoFilterClass {
+    GstElementClass parent_class;
+
+} GstVideoFilterClass;
+
+#define GST_TYPE_VIDEO_FILTER       (gst_video_filter_get_type())
+
+#define GST_VIDEO_FILTER(obj) ()    (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_VIDEO_FILTER, GstVideoFilter))
+#define GST_VIDEO_FILTER_CLASS()    (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_VIDEO_FILTER, GstVideoFilterClass))
+
+#define GST_IS_VIDEO_FILTER(obj)    (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_VIDEO_FILTER))
+#define GST_IS_VIDEO_FILTER_CLASS   (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_VIDEO_FILTER))
+
+GType gst_video_filter_get_type (void);
+
+G_END_DECLS
+
+#endif // _GST_VIDEO_FILTER_H_
