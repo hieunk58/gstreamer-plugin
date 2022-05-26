402375cb6646386b43ac5c18514187da850738f1
hieunk <khachieunk@gmail.com>
2022-05-26 12:22:43 +0200

    add pad template

--- src/gstvideofilter.c
+++ src/gstvideofilter.c
@@ -2,12 +2,35 @@
 
 static void gst_video_filter_class_init (GstVideoFilterClass *kclass);
 
+/* Pad templates */
+static GstStaticPadTemplate src_factory =
+GST_STATIC_PAD_TEMPLATE (
+    "src",
+    GST_PAD_SRC,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("ANY") // TODO specify supported video type
+);
+
+static GstStaticPadTemplate sink_factory =
+GST_STATIC_PAD_TEMPLATE (
+    "sink",
+    GST_PAD_SINK,
+    GST_PAD_ALWAYS,
+    GST_STATIC_CAPS ("ANY") // TODO specify supported video type
+);
+
 static void
 gst_video_filter_class_init (GstVideoFilterClass *kclass)
 {
     GST_DEBUG ("gst_video_filter_class_init");
 
     GstElementClass *element_class = GST_ELEMENT_CLASS (klass);
+
+    gst_element_class_add_pad_template (element_class,
+    gst_static_pad_template_get (&src_factory));
+
+    gst_element_class_add_pad_template (element_class,
+    gst_static_pad_template_get (&sink_factory));
 
     gst_element_class_set_static_metadata (element_class,
         "A video plugin",
